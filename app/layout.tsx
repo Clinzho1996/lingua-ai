9c03f245c0607c95f5e43e56b88aa8eb4cb37d96
Confidence Emonena <confidinho@yahoo.com>
2024-07-12 18:45:36 +0100

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable @next/next/no-sync-scripts */
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "LinguaAI",
+  description:
+    "AI Voice translator app to any language | Developed by Confidence Emonena",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <script src="./node_modules/preline/dist/preline.js"></script>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
